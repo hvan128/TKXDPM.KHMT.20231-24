c9acd9f750757fecc88dd2007fa694e38e18f502
Tuan Vu <vuanhtuan1407@gmail.com>
2023-11-27 14:40:43 +0700

    init

---
+++ src/entity/payment/PaymentTransaction.java
@@ -0,0 +1,32 @@
+package entity.payment;
+
+public class PaymentTransaction {
+    private String errorCode;
+    private String transactionId;
+    private String transactionContent;
+    private int amount;
+    private String createdAt;
+
+    public PaymentTransaction(String errorCode, String transactionId, String transactionContent,
+                              int amount, String createdAt) {
+        super();
+        this.errorCode = errorCode;
+
+        this.transactionId = transactionId;
+        this.transactionContent = transactionContent;
+        this.amount = amount;
+        this.createdAt = createdAt;
+    }
+
+
+    /**
+     * @return String
+     */
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public String getTransactionContent() {
+        return transactionContent;
+    }
+}
